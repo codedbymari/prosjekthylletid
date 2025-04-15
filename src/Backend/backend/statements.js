22470584f21fd297e3ebdf4cdfc2645e7d21617c
Rabia4012 <143596301+Rabia4012@users.noreply.github.com>
2025-04-15 16:05:37 +0200

    Update statements.js
    testfil

--- src/Backend/backend/statements.js
+++ src/Backend/backend/statements.js
@@ -1,14 +1,1 @@
-const db = require('better-sqlite3')('database.db')
 
-const createTable = () => {
-    const sql = `
-        CREATE TABLE bøker(
-            ISBN INTEGER PRIMARY KEY,
-            Navn TEXT NOT NULL,
-            Alder INEGER   
-        )
-    `
-    db.prepare(sql).run()
-}
-
-createTable
