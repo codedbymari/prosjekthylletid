1fa3515677e7787aa85b6a3b130aba97b2052950
Marjama Saiddatter Hassan <103361407+Marjama98@users.noreply.github.com>
2025-05-22 09:29:34 +0200

    slettet gjentakende kode

--- src/components/borrowers/BorrowerDashboard.js
+++ src/components/borrowers/BorrowerDashboard.js
@@ -552,12 +552,7 @@ function BorrowerDashboard() {
                   onKeyDown={handleQuickSearchKeyDown}
                   className="search-input"
                 />
-                <button type="submit" className="search-button">
-                  <svg className="search-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                  </svg>
-                  Søk
-                </button>
+  
               </div>
             </div>
 
